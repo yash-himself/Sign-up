46f77f7ab30672e29c1537749b5647f54d13330e
Yash Gupta <yashgupta27690@gmail.com>
2023-07-29 16:01:48 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,49 @@
+//checking if user want to go on profile page without sign up if true return user to sign up page
+let file = window.location.href.split('/')
+let logged = localStorage.getItem('user')
+if (logged == null && file[file.length - 1] == 'home.html') {
+    window.open('index.html', '_self')
+}
+
+
+// taking user information from local Storage
+let user = JSON.parse(localStorage.getItem('user'))
+let token = Math.round(user.token)
+
+
+
+// rendering profile
+console.log(user)
+let profileBox = document.createElement('div');
+profileBox.id = 'container'
+profileBox.innerHTML = `
+<h2>Profile</h2>
+<img src="profile.png">
+<div>
+<div class="div"><b>Name: </b> ${user.name}</div>
+<div class="div"><b>Email: </b> ${user.email}</div>
+<div class="div"><b>Your Password: </b> ${user.password}</div>
+<div class="div"><b>Token: </b> ${token}</div>
+
+</div>
+<button id = logOut>LOG OUT</button>
+`
+
+let body = document.querySelector('body')
+body.appendChild(profileBox);
+
+
+
+// log out functionality
+let logOut = document.getElementById('logOut');
+
+logOut.addEventListener('click', (e) => {
+    e.preventDefault()
+    // clearing storage
+    localStorage.clear()
+    // redirecting to sign up page
+    window.open('/index.html', '_self')
+})
+
+
+
---
+++ signUp.js
@@ -0,0 +1,57 @@
+// initializing variable 
+let Name
+let email
+let password
+let confirmPass
+let submit = document.getElementById('submit')
+
+
+//checking if user want to go on signup page while logged in  if true return user to profile page
+let file = window.location.href.split('/')
+let logged = localStorage.getItem('user')
+if (logged != null && file[file.length - 1] == 'index.html') {
+    window.open('home.html', '_self')
+}
+
+
+
+
+// login process
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    Name = document.getElementById('name').value.trim()
+    email = document.getElementById('email').value.trim()
+    password = document.getElementById('password').value.trim()
+    confirmPass = document.getElementById('conPassword').value.trim()
+// checking if any field is empty
+    if (Name === '' || email === '' || password.length === '' || confirmPass === '') {
+        alert('All fields are mandetory !')
+    }
+// checking if password length is less than 4
+    else if (password.length <= 3) { alert('Password length shoud be greater than 3') }
+// checking if password is not matching with confirm password
+    else if (password != confirmPass) { alert('incorrect password details !') }
+// if everything is good to go .. processing further to sign up
+    else {
+        let token = Math.random() * 1000000
+        let newObj = { 'name': Name, 'email': email, 'password': password, 'token': token }
+
+        localStorage.setItem('user', JSON.stringify(newObj))
+        document.querySelector('form').reset();
+        alert('Sign up Success...')
+
+// redirecting on successful sign up
+        window.open('/home.html', '_self')
+
+
+    }
+
+})
+
+
+
+
+
+
+
+
