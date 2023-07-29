b3bf1b892a9b39f2b1cd01ebdaa8d9854e56bd61
Yash Gupta <yashgupta27690@gmail.com>
2023-07-29 16:23:10 +0530

    Update home.js

--- home.js
+++ home.js
@@ -42,7 +42,7 @@ logOut.addEventListener('click', (e) => {
     // clearing storage
     localStorage.clear()
     // redirecting to sign up page
-    window.open('/index.html', '_self')
+    window.open('/Sign-up', '_self')
 })
 
 
